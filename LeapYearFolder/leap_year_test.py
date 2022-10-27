03bc504bb0977f9f7c8060c470e476805f2a37fa
Eirik Grandahl <112081332+eirikjg46@users.noreply.github.com>
2022-10-27 14:21:13 +0200

    Add files via upload

---
+++ LeapYearFolder/leap_year_test.py
@@ -0,0 +1,17 @@
+from LeapYearFolder.leap_year import isLeapYear
+
+# Divisible by 4 but not divisible by 100.
+def test_isLeapYear_div4ButNot100() -> None:
+    assert isLeapYear(2020)
+
+# Divisible by 400.
+def test_iLeapYear_div400() -> None:
+    assert isLeapYear(2000)
+
+# Not divisible by 4.
+def test_isLeapYear_notDiv4() -> None:
+    assert not isLeapYear(1999)
+
+# Divisible by 100, but not by 400.
+def test_isLeapYear_div100Not400() -> None:
+    assert not isLeapYear(1700)
